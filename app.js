291aa63f866078590aac65c8ea99109343d1341d
CornetS28 <corsam28@gmail.com>
2020-05-26 11:00:51 -0400

    set all the class structure

--- app.js
+++ app.js
@@ -7,3 +7,27 @@ const cartItems = document.querySelector(".cart-items");
 const cartContent = document.querySelector(".cart-content");
 const cartTotal = document.querySelector(".cart-total");
 const productsDOM = document.querySelector("products-center");
+
+// cart
+let cart = [];
+
+
+// getting all the products
+class Products {
+
+}
+
+// displaying all the products
+class UI {
+
+}
+
+// local storage
+class Storage {
+
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const ui = new UI()
+  const products = new Products()
+}) 
